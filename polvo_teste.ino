4efe0458429ff4184c0b6addcdb8d54f48b2b524
George Rappel <georgerappel@ufrj.br>
2015-10-21 11:40:48 -0200

    Suporte para vários fans

--- polvo_teste.ino
+++ polvo_teste.ino
@@ -1,13 +1,21 @@
 /*
-Sinal 1 - Azul
-Sinal 2 - Laranja
-Positivo - Branco com Azul
-Negativo - Branco com Laranja
+ * Developed by: George Rappel.
+ * george.concei@hotmail.com
+ */
+
+/* PINS E CORES
+ * Sinal 1 - Azul
+ * Sinal 2 - Laranja
+ * Positivo - Branco com Azul
+ * Negativo - Branco com Laranja
 */
 
+// ATUALIZAR AS DUAS VÁRIAVEIS ABAIXO.
+int totalFans = 2;
+int fansPins[] = {7, 8};
+// ATUALIZAR AS DUAS VARIÁVEIS ACIMA.
+
 int pinSensor = 0;
-int pin1 = 7;
-int pin2 = 8;
 int count = 0, limiteLoop = 5;
 int delayPadrao = 700;
 int sensorMinimo = 400, sensorMedio = 800;
@@ -15,8 +23,8 @@ int sensorMinimo = 400, sensorMedio = 800;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(pin1, OUTPUT);
-  pinMode(pin2, OUTPUT);
+  for(int i = 0; i < totalFans; i++)
+    pinMode(fansPins[i], OUTPUT);
   desligaTodos();
 }
 
@@ -41,22 +49,14 @@ void halfWay(){
   delay(delayPadrao);  
 }
 
-void liga(int pin){
-  digitalWrite(pin, HIGH);
-}
-
-void desliga(int pin){
-  digitalWrite(pin, LOW);
-}
-
 void desligaTodos(){
   Serial.println("desligando todos");
-  digitalWrite(pin1, LOW);
-  digitalWrite(pin2, LOW); 
+  for(int i = 0; i < totalFans; i++)
+    digitalWrite(fansPins[i], LOW);
 }
 
 void ligaTodos(){
   Serial.println("ligando todos");
-  digitalWrite(pin1, HIGH);
-  digitalWrite(pin2, HIGH); 
+  for(int i = 0; i < totalFans; i++)
+    digitalWrite(fansPins[i], HIGH);
 }
