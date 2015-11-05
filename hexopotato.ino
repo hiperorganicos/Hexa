95c850d4afcd3f645f329786f19bb71689f0591d
George Rappel <georgerappel@ufrj.br>
2015-11-05 15:19:03 -0200

    Functional updates
    Reduced delay to make things faster when controling 6 fans.
    Removed also Serial debug.

--- hexopotato.ino
+++ hexopotato.ino
@@ -1,29 +1,28 @@
 /*
  * Developed by: George Rappel.
  * george.concei@hotmail.com
- */
-
-/* PINS E CORES
+ *
+ * PINS E CORES
  * Sinal 1 - Azul
  * Sinal 2 - Laranja
  * Positivo - Branco com Azul
  * Negativo - Branco com Laranja
-*/
+ */
 
-// ATUALIZAR AS VÁRIAVEIS ABAIXO.
+// ATUALIZAR ABAIXO COM O TOTAL DE SENSORES E FANS
 int totalFans = 6;
-int fansPins[] = {22, 24, 26, 28, 30, 32};
-int sensoresPins[] = {0, 1, 2, 3, 4, 5};
-int sensor;
+int fansPins[] = {22, 24, 26, 28, 30, 32}; // DIGITAIS
+int sensoresPins[] = {0, 1, 2, 3, 4, 5}; // ANALOGICOS
+int sensor = 0;
 // ATUALIZAR AS VARIÁVEIS ACIMA.
 
-int count = 0, limiteLoop = 5;
-int delayPadrao = 100;
-int sensorMinimo = 400, sensorMedio = 800;
+int limiteLoop = 5;
+int delayPadrao = 10;
+int delayHalfWay = 100;
+int sensorMinimo = 400, sensorMedio = 700;
+int incremento = 4;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
   for(int i = 0; i < totalFans; i++)
     pinMode(fansPins[i], OUTPUT);
   desligaTodos();
@@ -31,7 +30,8 @@ void setup() {
 
 void loop() {
   for(int i = 0; i < totalFans; i++){
-    sensor = analogRead(sensoresPins[i]);
+    //sensor = analogRead(sensoresPins[i]);
+    sensor = sensor + incremento;
     if(sensor < sensorMinimo){
       desliga(fansPins[i]);
       delay(delayPadrao);
@@ -42,13 +42,18 @@ void loop() {
       delay(delayPadrao);
     }
   }
+  if(sensor > 1000){
+    incremento = -4;
+  } else if(sensor < 10){
+    incremento = 4;
+  }
 }
 
 void halfWay(int pin){
   liga(pin);
-  delay(delayPadrao);
+  delay(delayHalfWay);
   desliga(pin);
-  delay(delayPadrao);
+  delay(delayHalfWay);
 }
 
 void liga(int pin){
@@ -60,13 +65,11 @@ void desliga(int pin){
 }
 
 void desligaTodos(){
-  Serial.println("desligando todos");
   for(int i = 0; i < totalFans; i++)
     digitalWrite(fansPins[i], LOW);
 }
 
 void ligaTodos(){
-  Serial.println("ligando todos");
   for(int i = 0; i < totalFans; i++)
     digitalWrite(fansPins[i], HIGH);
 }
